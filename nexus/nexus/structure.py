e6ea1fc1765538f4733f6d4795e952eb95f7535d
Brock Dyer <brockdyer03@gmail.com>
2026-05-20 14:51:45 -0400

    Nexus: Update `Structure.rotate()` docstring

--- nexus/nexus/structure.py
+++ nexus/nexus/structure.py
@@ -2098,32 +2098,36 @@ class Structure(Sobj):
         Parameters
         ----------
         r : ArrayLike of float with shape (3,3) or ArrayLike of float shape (3,) or str
-            If a 3x3 matrix, then code executes rotation consistent with this matrix -- 
-            it is assumed that the matrix acts on a column-major vector (eg, v'=Rv)
-            If a three-dimensional array, then the operation of the function depends
-            on the input type of ``rp`` in the following ways:
+            If a 3x3 matrix, then code executes rotation consistent with this
+            matrix -- it is assumed that the matrix acts on a column-major
+            vector (eg, v'=Rv). If a three-dimensional array, then the operation
+            of the function depends on the input type of ``rp`` in the
+            following ways:
 
-            1. If ``rp`` is a scalar, then ``rp`` is assumed to be an angle and a rotation 
-               of ``rp`` is made about the axis defined by ``r``
+            1. If ``rp`` is a scalar, then ``rp`` is assumed to be an angle and
+               a rotation of ``rp`` is made about the axis defined by ``r``
             2. If ``rp`` is a vector, then ``rp`` is assumed to be an axis and a 
                rotation is made such that ``r`` aligns with ``rp``
-            3. If ``rp`` is a ``str``, then the rotation is such that ``r`` aligns with the
-               axis given by the str ``('x', 'y', 'z', 'a0', 'a1', or 'a2')``
+            3. If ``rp`` is a ``str``, then the rotation is such that ``r``
+               aligns with the axis given by the str
+               ``('x', 'y', 'z', 'a0', 'a1', or 'a2')``
 
             If a ``str`` then the axis, ``r``, is defined by the input label
             (e.g. 'x', 'y', 'z', 'a1', 'a2', or 'a3') and the operation of the
-            function depends on the input type of ``rp`` in the same ways as above
+            function depends on the input type of ``rp`` in the same ways as
+            above.
         rp : ArrayLike of float with shape (3) or str, optional
-            If a 3-dimensional vector is given, then ``rp`` is assumed to be an axis and a rotation is made
-            such that the axis ``r`` is aligned with ``rp``.
-            If a str, then ``rp`` is assumed to be an angle and a rotation about the axis defined by ``r`` 
-            is made by an angle ``rp``
-            If a str is given, then ``rp`` is assumed to be an axis defined by the given label
-            (e.g. 'x', 'y', 'z', 'a1', 'a2', or 'a3') and a rotation is made such that the axis ``r`` 
-            is aligned with ``rp``.
+            If a 3-dimensional vector is given, then ``rp`` is assumed to be an
+            axis and a rotation is made such that the axis ``r`` is aligned with
+            ``rp``.
+            If a str, then ``rp`` is assumed to be an angle and a
+            rotation about the axis defined by ``r`` is made by an angle ``rp``
+            If a str is given, then ``rp`` is assumed to be an axis defined by
+            the given label (e.g. 'x', 'y', 'z', 'a1', 'a2', or 'a3') and a
+            rotation is made such that the axis ``r`` is aligned with ``rp``.
         passive : bool, default=False
-            If `True`, perform a passive rotation
-            If `False`, perform an active rotation
+            If ``True``, perform a passive rotation.
+            If ``False``, perform an active rotation.
         units : str, default="radians"
             Units of ``rp``, if ``rp`` is given as an angle (scalar)
         check : bool, default=True
